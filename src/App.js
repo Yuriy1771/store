d21a850f36457534bd62928bf98862e8c35293c4
Yuriy <dementev.yuriy@list.ru>
2023-01-26 15:12:46 +0300

    Сделал верстку

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import classes from './index.module.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={classes.wrapper}>
+            <header>
+                <div className={classes.headerLeft}>
+                    <img src='/imgs/header/logoStore.png'/>
+                    <div>
+                        <h3>T-shirts store</h3>
+                        <p>Store of the best t-shirts</p>
+                    </div>
+                </div>
+                <ul className={classes.headerRight}>
+                    <li>
+                        <img className={classes.logoCart} src='/imgs/header/cart.png'/>
+                        <span>3 645 rub.</span></li>
+                    <li>
+                        <img className={classes.logoProfile} src='/imgs/header/profile.png'/>
+                    </li>
+                </ul>
+            </header>
+
+            <div className={classes.content}>
+                <h1>All t-shirts</h1>
+                <div className={classes.tshirts}>
+
+                <div className={classes.card}>
+                    <div className={classes.tShirts}>
+                        <img src='/imgs/t-shirts/1.png' alt="product"/>
+                    </div>
+                    <h5>T-SHIRT ROUND NECK SS MONSTERS WITH CRYSTALS</h5>
+                    <div className={classes.cardBottom}>
+                        <div className={classes.priceProduct}>
+                            <span>Price:</span>
+                            <b>6 450 rub.</b>
+                        </div>
+                        <button>
+                            <img src='/imgs/content/addTshirt.png' alt="plus"/>
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        </div>
+    );
 }
 
 export default App;
